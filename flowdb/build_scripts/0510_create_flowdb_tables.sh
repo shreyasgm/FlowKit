493e27b314150be0c9c0a8a7dff058016e94e355
Maximilian Albert <maximilian.albert@flowminder.org>
2019-09-05 17:38:05 +0100

    Add python script to create events.call table

---
+++ flowdb/build_scripts/0510_create_flowdb_tables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -euo pipefail
+
+python3 /docker-entrypoint-initdb.d/0511_create_flowdb_tables.py
---
+++ flowdb/build_scripts/0511_create_flowdb_tables.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import os
+from sqlalchemy import (
+    create_engine,
+    MetaData,
+    Table,
+    Column,
+    Text,
+    Boolean,
+    Integer,
+    TIMESTAMP,
+    Numeric,
+    String,
+)
+from sqlalchemy.schema import CreateSchema
+
+postgres_user = os.environ["POSTGRES_USER"]
+postgres_password = os.environ["POSTGRES_PASSWORD"]
+
+conn_str = "postgresql://{}:{}@localhost:5432/flowdb_revised_schema".format(
+    postgres_user, postgres_password
+)
+engine = create_engine(conn_str)
+
+print("[DDD] conn_str={}".format(conn_str))
+
+if not engine.dialect.has_schema(engine, "events"):
+    engine.execute(CreateSchema("events"))
+
+metadata = MetaData()
+
+events_calls = Table(
+    "calls",
+    metadata,
+    Column("id", Text()),
+    Column("outgoing", Boolean()),
+    Column("datetime", TIMESTAMP(timezone=True), nullable=False),
+    Column("duration", Numeric()),
+    Column("network", Text()),
+    Column("msisdn", Text(), nullable=False),
+    Column("msisdn_counterpart", Text()),
+    Column("location_id", Text()),
+    Column("imsi", Text()),
+    Column("imei", Text()),
+    Column("tac", Numeric(8)),
+    Column("operator_code", Numeric()),
+    Column("country_code", Numeric()),
+    schema="events",
+)
+
+metadata.create_all(bind=engine)
